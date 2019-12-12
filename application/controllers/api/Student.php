7548964f410dc7dba086b24cb8746579794ec220
Online Web Tutor <learnbywatchhub@gmail.com>
2019-12-12 06:32:39 +0530

    Security, Delete & PUT API methods

--- application/controllers/api/Student.php
+++ application/controllers/api/Student.php
@@ -10,6 +10,8 @@ class Student extends REST_Controller{
     //load database
     $this->load->database();
     $this->load->model(array("api/student_model"));
+    $this->load->library(array("form_validation"));
+    $this->load->helper("security");
   }
 
   /*
@@ -23,19 +25,128 @@ class Student extends REST_Controller{
   public function index_post(){
     // insert data method
 
-    echo "This is POST Method";
+    //print_r($this->input->post());die;
+
+    // collecting form data inputs
+    $name = $this->security->xss_clean($this->input->post("name"));
+    $email = $this->security->xss_clean($this->input->post("email"));
+    $mobile = $this->security->xss_clean($this->input->post("mobile"));
+    $course = $this->security->xss_clean($this->input->post("course"));
+
+    // form validation for inputs
+    $this->form_validation->set_rules("name", "Name", "required");
+    $this->form_validation->set_rules("email", "Email", "required|valid_email");
+    $this->form_validation->set_rules("mobile", "Mobile", "required");
+    $this->form_validation->set_rules("course", "Course", "required");
+
+    // checking form submittion have any error or not
+    if($this->form_validation->run() === FALSE){
+
+      // we have some errors
+      $this->response(array(
+        "status" => 0,
+        "message" => "All fields are needed"
+      ) , REST_Controller::HTTP_NOT_FOUND);
+    }else{
+
+      if(!empty($name) && !empty($email) && !empty($mobile) && !empty($course)){
+        // all values are available
+        $student = array(
+          "name" => $name,
+          "email" => $email,
+          "mobile" => $mobile,
+          "course" => $course
+        );
+
+        if($this->student_model->insert_student($student)){
+
+          $this->response(array(
+            "status" => 1,
+            "message" => "Student has been created"
+          ), REST_Controller::HTTP_OK);
+        }else{
+
+          $this->response(array(
+            "status" => 0,
+            "message" => "Failed to create student"
+          ), REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
+        }
+      }else{
+        // we have some empty field
+        $this->response(array(
+          "status" => 0,
+          "message" => "All fields are needed"
+        ), REST_Controller::HTTP_NOT_FOUND);
+      }
+    }
+
+    /*$data = json_decode(file_get_contents("php://input"));
+
+    $name = isset($data->name) ? $data->name : "";
+    $email = isset($data->email) ? $data->email : "";
+    $mobile = isset($data->mobile) ? $data->mobile : "";
+    $course = isset($data->course) ? $data->course : "";*/
+
+
   }
 
   // PUT: <project_url>/index.php/student
   public function index_put(){
     // updating data method
-    echo "This is PUT Method";
+    //echo "This is PUT Method";
+    $data = json_decode(file_get_contents("php://input"));
+
+    if(isset($data->id) && isset($data->name) && isset($data->email) && isset($data->mobile) && isset($data->course)){
+
+      $student_id = $data->id;
+      $student_info = array(
+        "name" => $data->name,
+        "email" => $data->email,
+        "mobile" => $data->mobile,
+        "course" => $data->course
+      );
+
+      if($this->student_model->update_student_information($student_id, $student_info)){
+
+          $this->response(array(
+            "status" => 1,
+            "message" => "Student data updated successfully"
+          ), REST_Controller::HTTP_OK);
+      }else{
+
+        $this->response(array(
+          "status" => 0,
+          "messsage" => "Failed to update student data"
+        ), REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
+      }
+    }else{
+
+      $this->response(array(
+        "status" => 0,
+        "message" => "All fields are needed"
+      ), REST_Controller::HTTP_NOT_FOUND);
+    }
   }
 
   // DELETE: <project_url>/index.php/student
   public function index_delete(){
     // delete data method
-    echo "This is DELETE Method";
+    $data = json_decode(file_get_contents("php://input"));
+    $student_id = $this->security->xss_clean($data->student_id);
+
+    if($this->student_model->delete_student($student_id)){
+      // retruns true
+      $this->response(array(
+        "status" => 1,
+        "message" => "Student has been deleted"
+      ), REST_Controller::HTTP_OK);
+    }else{
+      // return false
+      $this->response(array(
+        "status" => 0,
+        "message" => "Failed to delete student"
+      ), REST_Controller::HTTP_NOT_FOUND);
+    }
   }
 
   // GET: <project_url>/index.php/student
--- application/models/api/Student_model.php
+++ application/models/api/Student_model.php
@@ -15,6 +15,24 @@ class Student_model extends CI_Model{
 
     return $query->result();
   }
+
+   public function insert_student($data = array()){
+
+       return $this->db->insert("tbl_students", $data);
+   }
+
+   public function delete_student($student_id){
+
+     // delete method
+     $this->db->where("id", $student_id);
+     return $this->db->delete("tbl_students");
+   }
+
+   public function update_student_information($id, $informations){
+
+      $this->db->where("id", $id);
+      return $this->db->update("tbl_students", $informations);
+   }
 }
 
  ?>
